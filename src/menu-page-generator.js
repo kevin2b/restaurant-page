9c1386a250ec467d6b02b7af0055efbc05e8bdb3
kevin2b <25062561+kevin2b@users.noreply.github.com>
2023-12-01 13:39:20 -0500

    Add Menu content

--- src/menu-page-generator.js
+++ src/menu-page-generator.js
@@ -1,4 +1,5 @@
 import Content from './dom-generator.js';
+import {createContentCard} from './dom-generator.js';
 
 
 function MenuPage (){
@@ -11,7 +12,32 @@ function MenuPage (){
   subTitle.classList.add('content-subtitle');
   contentBody.appendChild(subTitle);
   
+  contentBody.appendChild(createMenuCard("Food Item A", 10.99));
+  contentBody.appendChild(createMenuCard("Food Item B - Special Edition", 1092.99));
+  contentBody.appendChild(createMenuCard("Food Item E", 1.99));
+  contentBody.appendChild(createMenuCard("Drink M", 0.99));
+  
   return {menuContent};
+}
+
+
+function createMenuCard(foodDesc, foodPrice){
+  const menuCard = createContentCard();
+  menuCard.cardLeftSide.classList.add('menu-card');
+  
+  const food = document.createElement('span');
+  food.classList.add('card-food');
+  food.innerText = foodDesc;
+  
+  const price = document.createElement('span');
+  price.classList.add('card-price');
+  price.innerText = '$' + foodPrice;
+  
+  menuCard.cardLeftSide.appendChild(food);
+  menuCard.cardLeftSide.appendChild(price);
+  menuCard.cardRightSide.innerText = 'Placeholder Food Image';
+  
+  return menuCard.cardWrapper;
 }
 
 export default MenuPage;
