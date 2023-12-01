3e3895a6fd4b49c5dbdd851d39c888d1d94912e6
kevin2b <25062561+kevin2b@users.noreply.github.com>
2023-12-01 13:06:35 -0500

    add Home page content

--- src/dom-generator.js
+++ src/dom-generator.js
@@ -9,11 +9,33 @@ function Content(){
   content.appendChild(contentBody);
   
   const restaurantName = document.createElement('div');
-  contentBody.classList.add('content-title');
+  restaurantName.classList.add('content-title');
   contentBody.appendChild(restaurantName);
   restaurantName.innerText = RESTAURANT_NAME;
   
   return {content, contentBody};
 }
 
+//Create a card with two sides
+function createContentCard (){
+  
+  const cardWrapper = document.createElement('div');
+  cardWrapper.classList.add('content-card-wrapper');
+  
+  const card = document.createElement('div');
+  card.classList.add('content-card');
+  
+  const cardLeftSide = document.createElement('div');
+  cardLeftSide.classList.add('content-card-left');
+  
+  const cardRightSide = document.createElement('div');
+  cardRightSide.classList.add('content-card-right');
+  
+  cardWrapper.appendChild(card);
+  card.appendChild(cardLeftSide);
+  card.appendChild(cardRightSide);
+  return {cardWrapper, card, cardLeftSide, cardRightSide};
+}
+
 export default Content;
+export {createContentCard};
--- src/home-page-generator.js
+++ src/home-page-generator.js
@@ -1,4 +1,5 @@
 import Content from './dom-generator.js';
+import {createContentCard} from './dom-generator.js';
 
 
 function HomePage (){
@@ -10,6 +11,12 @@ function HomePage (){
   subTitle.textContent = 'Home';
   subTitle.classList.add('content-subtitle');
   contentBody.appendChild(subTitle);
+  
+  const welcomeCard = createContentCard();
+  welcomeCard.cardLeftSide.classList.add('home-card');
+  welcomeCard.cardLeftSide.innerText = "Welcome to Spooky Restaurant. Spooky Restaurant has been serving the community for over 50 years providing quality food and a spooky experience.";
+  welcomeCard.cardRightSide.innerText = "Image Placeholder";
+  contentBody.appendChild(welcomeCard.cardWrapper);
   
   return {homeContent};
 }
